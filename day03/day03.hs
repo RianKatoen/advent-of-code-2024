f585716a2d14a5af779522fe308acd84fec40cb3
Rian Katoen <riankatoen@gmail.com>
2024-12-03 17:53:15 +0100

    Day03 stolen split from Hoogle :). Way faster.

--- day03/day03.hs
+++ day03/day03.hs
@@ -1,16 +1,13 @@
 import Data.Char (isDigit)
-import Debug.Trace (traceShow)
+import Data.List (isPrefixOf, findIndex, tails)
+import Data.Maybe (isNothing, fromJust)
 
 -- Let's try and do this without regex...
 split :: Char -> String -> [String]
-split _ [] = [""]
-split c x = do
-    let until = takeUntil [c] x
-    if length until >= length x
-        then [until]
-    else do
-        let y = dropUntil [c] x
-        until : split c y
+split c s = case rest of
+                []     -> [chunk]
+                _:rest -> chunk : split c rest
+  where (chunk, rest) = break (==c) s
 
 isNumber :: String -> Bool
 isNumber [] = False
@@ -23,47 +20,31 @@ isLegit x
     | length x > 7 = False -- No more than 3 digits.
     | otherwise = all (\y -> isNumber y && not (null y) && (length y <= 3)) (split ',' x)
 
-dropUntil :: String -> String -> String
-dropUntil a x
-    | length a > length x = []
-    | length x == 1 = []
-    | a == take (length a) x = drop (length a) x
-    | otherwise = dropUntil a (tail x)
-
-takeUntil :: String -> String -> String
-takeUntil a x
-    | length a > length x = x
-    | a == take (length a) x = []
-    | otherwise = take 1 x ++ takeUntil a (tail x)
-
-capture :: String -> String -> String -> String
-capture a b x = do
-    let start = takeUntil a x
-    let remainder = takeUntil b start
-    if length start == length remainder
-        then []
-    else
-        remainder
-
-captureAll :: (String -> Bool) -> String -> String -> String -> [String]
-captureAll f a b x
-    | null x  = []
-    | length a + length b >= length x = []
-    | otherwise = do
-        let captured = capture a b x
-        if f captured
-            then captured : captureAll f a b (dropUntil (a ++ captured ++ b) x)
-        else
-            captureAll f a b (dropUntil a x)
-
 calculate :: [String] -> Int
 calculate x = product $ map read x
 
+captureNew :: (String -> Bool) -> String -> String -> String -> [String]
+captureNew f a b x = do
+    let ix_a = findIndex (isPrefixOf a) (tails x)
+    if isNothing ix_a
+        then []
+    else do
+        let remainder = drop (fromJust ix_a + length a) x
+        let ix_b = findIndex (isPrefixOf b) (tails remainder)
+        if isNothing ix_b
+            then []
+        else do
+            let captured = take (fromJust ix_b) remainder
+            if f captured
+                then captured : captureNew f a b (drop (fromJust ix_b) remainder)
+            else
+                captureNew f a b remainder
+
 part1 :: String -> Int
-part1 x = sum $ map (calculate . split ',') (captureAll isLegit "mul(" ")" x)
+part1 x = sum $ map (calculate . split ',') (captureNew isLegit "mul(" ")" x)
 
 part2 :: String -> Int
-part2 x = sum $ map part1 $ captureAll (const True) "do()" "don't()" ("do()" ++ x ++ "don't()")
+part2 x = sum $ map part1 $ captureNew (const True) "do()" "don't()" ("do()" ++ x ++ "don't()")
 
 main = do
     let exampleInput1 = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
