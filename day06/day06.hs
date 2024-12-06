ffd516835e498d3e72cc38789211c7494e418afe
Rian Katoen <riankatoen@gmail.com>
2024-12-06 22:33:21 +0100

    Bad rian.

--- day06/day06.hs
+++ day06/day06.hs
@@ -1,5 +1,5 @@
-import Debug.Trace (traceShow)
-import Data.List (findIndex, elemIndex, nub)
+import Debug.Trace (traceShow, traceShowId)
+import Data.List (findIndex, elemIndex, nub, sort)
 import Data.Maybe (fromJust)
 import Prelude hiding (Right, Left)
 import Control.Arrow (ArrowLoop(loop))
@@ -8,7 +8,7 @@ import Control.Monad (guard)
 data Direction = Left | Right | Up | Down
     deriving (Eq, Show)
 
-data Pixel = Dot | Hash
+data Pixel = Dot | Obj
     deriving (Eq, Show)
 
 split :: Eq a => a -> [a] -> [[a]]
@@ -22,7 +22,7 @@ parseMap filePath = do
     contents <- readFile filePath
     let elfMap = lines contents
     let (x, y) = head $ filter (\(x, y) -> y /= Nothing) $ zip [0..] (map (elemIndex '^') elfMap)
-    return ((x, fromJust y), map (map (\x -> if x == '#' then Hash else Dot)) elfMap)
+    return ((x, fromJust y), map (map (\x -> if x == '#' then Obj else Dot)) elfMap)
 
 rotate :: Direction -> Direction
 rotate d = case d of
@@ -53,17 +53,17 @@ step elfMap d (x, y)
         | otherwise                     = (rotate d, (x, y), False)
     where (dx, dy) = move d (x, y)
 
-play :: [[Pixel]] -> Direction -> (Int, Int) -> [(Direction, (Int, Int))] -> [(Direction, (Int, Int))]
-play elfMap d (x, y) steps
-        | terminated                                = (d, (x, y)):steps
-        | (d, (x, y)) `elem` steps                  = steps
-        | otherwise                                 = play elfMap dd (dx, dy) ((d, (x, y)) : steps)
+play :: [[Pixel]] -> [(Direction, (Int, Int))] -> Direction -> (Int, Int) -> [(Direction, (Int, Int))]
+play elfMap steps d (x, y)
+        | terminated                                = steps ++ [(d, (x, y))]
+        | (d, (x, y)) `elem` steps                  = steps -- "Looping" condition.
+        | otherwise                                 = play elfMap (steps ++ [(d, (x, y))]) dd (dx, dy)
     where (dd, (dx, dy), terminated) = step elfMap d (x, y)
 
 getPath :: FilePath -> IO [(Direction, (Int, Int))]
 getPath filePath = do
     (loc, elfMap) <- parseMap filePath
-    return $ play elfMap Up loc []
+    return $ play elfMap [] Up loc
 
 part1 :: FilePath -> IO Int
 part1 filePath = do
@@ -71,11 +71,34 @@ part1 filePath = do
     let (directions, locs) = unzip stuff
     return $ length $ nub locs
 
+placeObstacle :: (Int, Int) -> [[Pixel]] -> [[Pixel]]
+placeObstacle (x, y) elfMap = do
+    let (a, b:bs) = splitAt x elfMap
+    let (aa, bb:bbs) = splitAt y b
+    let henk = aa ++ [Obj] ++ bbs
+    a ++ [henk] ++ bs
+
+getAllPossibleObstacles :: [[Pixel]] -> [(Direction, (Int, Int))] -> [(Int, (Int, Int))]
+getAllPossibleObstacles elfMap guardPath = filter (\(i, (x, y)) -> elfMap !! x !! y /= Obj) $ filter (\(d, loc) -> not $ isOutOfBounds elfMap loc) $ zipWith (curry (\(i, (d, loc)) -> (i, move d loc))) [0..] guardPath
+
+superplay :: [[Pixel]] -> [(Direction, (Int, Int))] -> (Int, Int) -> Int -> [(Direction, (Int, Int))]
+superplay elfMap guardPath loc i =
+        play (placeObstacle loc elfMap) (take i guardPath) a b
+    where (a, b) = guardPath !! i
+
 main = do
     putStrLn "\n--part1"
-    
+
     example1 <- part1 "example1.txt"
     print [example1, example1 - 41]
 
     answerPart1 <- part1 "input.txt"
     print [answerPart1, answerPart1 - 5444]
+
+    putStrLn "\n--part2"
+    (loc, elfMap) <- parseMap "input.txt"
+    let guardPath = play elfMap [] Up loc
+    let allObstacles = getAllPossibleObstacles elfMap guardPath
+
+    let henk = filter (\(l, (x, y)) -> not (isOutOfBounds elfMap (x, y))) $ map (\(l, (d, (x, y))) -> (l, move d (x, y))) $ map (\(i, l) -> (l, last $ superplay elfMap guardPath l $ max 0 0)) $ allObstacles
+    print $ length $ nub $ map (\(i, (l, _)) -> traceShow (i, l) l) $ zip [0..] henk
