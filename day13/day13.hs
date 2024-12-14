cfbe91b0e4d50d680a5c8da33b647ef03035eabe
Rian Katoen <riankatoen@gmail.com>
2024-12-14 11:19:15 +0100

    To lazy to do matrix inversion... Still had to do it for part 2 day 13.

--- day13/day13.hs
+++ day13/day13.hs
@@ -1,6 +1,6 @@
 import Debug.Trace (traceShowId)
 import Data.List (groupBy)
-import Data.Maybe (isJust, fromJust)
+import Data.Maybe (isJust, fromJust, mapMaybe)
 import qualified Data.Bifunctor
 
 data Arcade = Arcade { a :: (Int, Int), b :: (Int, Int), prize :: (Int, Int) }
@@ -20,40 +20,37 @@ parseLine str
          = (parseMove x, parseMove y)
     where parseMove = read @Int . tail . dropWhile (\c -> c /= '+' && c /= '=')
 
--- (<-->) :: (Int, Int) -> (Int, Int) -> (Int, Int)
 (<-->) (a, b) (x, y) = (a - x, b - y)
-(<//>) (a, b) (x, y) = (a `div` x, b `div` y)
 (</=>) (a, b) (x, y) = a /= x || b /= y
-(<<<) (a, b) (x, y) = a <= x && b <= y
-(<**>) (a, b) (x, y) = (a * x, b * y)
 
-(<-*->) z (a, b) = (a * z, b * z)
-(<-<-<) (a, b) z = a < z && b < z
+solve :: Arcade -> Maybe (Int, Int)
+solve (Arcade (a_x, a_y) (b_x, b_y) (p_x, p_y))
+        | ((p_x, p_y) <--> (a_x * n_a + b_x * n_b, a_y * n_a + b_y * n_b)) </=> (0, 0) = Nothing
+        | otherwise                                                                    = Just (n_a, n_b)
+    where d          = a_x * b_y - b_x * a_y
+          (n_a, n_b) = ((b_y * p_x - b_x * p_y) `div` d, (a_x * p_y - a_y * p_x) `div` d)
 
--- solveB :: Arcade -> Int -> Maybe Int
-getPossibleA :: Arcade -> [Int]
-getPossibleA (Arcade a _ p) = takeWhile (\n -> n <-*-> a <<< p) [0..]
+part1 :: [Arcade] -> Int
+part1 = sum . map ((uncurry (+) . (<**> (3, 1)) . fromJust) . Just) . mapMaybe solve
+    where (<**>) (a, b) (x, y) = (a * x, b * y)
 
-solveB :: Arcade -> Int -> Maybe Int
-solveB (Arcade a b p) n_a
-        | rem <-<-< 0                               = Nothing
-        | uncurry (/=) n_b                          = Nothing
-        | (rem <--> (fst n_b <-*-> b)) </=> (0, 0)   = Nothing
-        | otherwise                                 = Just $ fst n_b
-    where rem   = p <--> (n_a <-*-> a)
-          n_b   = rem <//> b
-
-getPossibleSolutions :: Arcade -> [(Int, Int)]
-getPossibleSolutions arcade = map (Data.Bifunctor.second fromJust) $ filter (\(_, b) ->  isJust b) $ map (\n_a -> (n_a, solveB arcade n_a)) $ getPossibleA arcade
-
--- part1 :: [Arcade] -> Int
-part1 = sum . map (uncurry (+) . (<**> (3, 1)) . head) . filter (/=[]) . map getPossibleSolutions
+part2 :: [Arcade] -> Int
+part2 = part1 . map correctPrize
+    where correctPrize (Arcade a b p) = Arcade a b ((<-+->) 10_000_000_000_000 p)
+          (<-+->) z (a, b) = (a + z, b + z)
 
 main = do
     exampleArcades <- parseArcades "example1.txt"
     inputArcades <- parseArcades "input.txt"
 
+    putStrLn "\npart 1"
     let example1 = part1 exampleArcades
     print [example1, example1 - 480]
     let answerPart1 = part1 inputArcades
-    print [answerPart1, answerPart1 - 480]
+    print [answerPart1, answerPart1 - 35997]
+
+    putStrLn "\npart 2"
+    let example2 = part2 exampleArcades
+    print [example2, example2 - 875318608908]
+    let answerPart2 = part2 inputArcades
+    print [answerPart2, answerPart2 - 82510994362072]
